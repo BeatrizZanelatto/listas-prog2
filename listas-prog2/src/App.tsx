e0949e4c9322776d281dd3e54f2d2fd1d4b7ac7b
BeatrizZanelatto <104568663+BeatrizZanelatto@users.noreply.github.com>
2023-09-12 14:09:38 +0000

    alterações

--- listas-prog2/src/App.tsx
+++ listas-prog2/src/App.tsx
@@ -1,87 +1,110 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 
 //Função principal
-export default function App(){
+export default function App() {
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const primeiraR = useRef(true);
 
   const [input, setInput] = useState("");
   const [tarefas, setTarefas] = useState<string[]>([])
 
   const [editarTarefa, setEditarTarefa] = useState({
     enabled: false,
-    tarefa:''
+    tarefa: ''
   })
-  
-  useEffect(() =>{
-   const tarefaSalva = localStorage.getItem("@cursoreact")
-   console.log(tarefaSalva);
-}, [])
+
+  useEffect(() => {
+    const tarefaSalva = localStorage.getItem("@cursoreact")
+    if (tarefaSalva) {
+      setTarefas(JSON.parse(tarefaSalva));
+    }
+  }, [])
+
+  useEffect(() => {
+    if (primeiraR.current) {
+      primeiraR.current = false;
+      return;
+    }
+    localStorage.setItem("@cursoreact", JSON.stringify(tarefas))
+    console.log("useEffect chamado!")
+  }, [tarefas])
 
 
-    function registrar(){
+  function registrar() {
+    if (!input) {
+      alert("Preencha o nome da sua tarefa")
+      return;
+    }
+
+    const registar = useCallback( () => {
       if(!input){
         alert("Preencha o nome da sua tarefa")
         return;
       }
-      
-      if(editarTarefa.enabled){
-        editarTarefaSalva();
-        return;
-      }
-
-      setTarefas(tarefas => [...tarefas, input])
-      setInput("")
-      localStorage.setItem("@cursoreact", JSON.stringify([...tarefas, input]))
     }
 
-    function editarTarefaSalva(){
-      const findIndexTarefa = tarefas.findIndex(tarefas => tarefas === editarTarefa.tarefa)
-      const todasTarefas = [...tarefas];
-
-      todasTarefas[findIndexTarefa]=input;
-      setTarefas(todasTarefas);
-      setEditarTarefa({
-        enabled: false,
-        tarefa: ''
-      })
-      setInput("")
-      localStorage.setItem("@cursoreact", JSON.stringify(todasTarefas))
-
+    if (editarTarefa.enabled) {
+      editarTarefaSalva();
+      return;
     }
 
-    function excluir(item: string){
-      const excluirTarefa = tarefas.filter(tarefas => tarefas !== item)
-      setTarefas(excluirTarefa)
-      localStorage.setItem("@cursoreact", JSON.stringify(excluirTarefa))
-    }
-    function editar(item: string){
-      setInput(item)
-      setEditarTarefa({
-        enabled:true,
-        tarefa: item
-      })
-    }
+    setTarefas(tarefas => [...tarefas, input])
+    setInput("")
+  }
 
-   return (
-      <div>
+  function editarTarefaSalva() {
+    const findIndexTarefa = tarefas.findIndex(tarefas => tarefas === editarTarefa.tarefa)
+    const todasTarefas = [...tarefas];
 
-        <button onClick={ () => setTeste(true) }> Clicar</button>
+    todasTarefas[findIndexTarefa] = input;
+    setTarefas(todasTarefas);
+    setEditarTarefa({
+      enabled: false,
+      tarefa: ''
+    })
+    setInput("")
+  }
+
+  function excluir(item: string) {
+    const excluirTarefa = tarefas.filter(tarefas => tarefas !== item)
+    setTarefas(excluirTarefa)
+  }
+  function editar(item: string) {
+    inputRef.current?.focus();
+
+    setInput(item)
+    setEditarTarefa({
+      enabled: true,
+      tarefa: item
+    })
+  }
+
+  const totalTarefas = useMemo(() =>{
+    return tarefas.length
+  },[tarefas])
+
+  return (
+      <div className='containerDiv'>
+
         <h1>Lista de tarefas</h1>
 
         <input
           placeholder="Digite uma tarefa..."
           value={input}
-          onChange={ (e) => setInput(e.target.value)}
+          onChange={(e) => setInput(e.target.value)}
+          ref={inputRef}
         />
         <button onClick={registrar}>{editarTarefa.enabled ? "Atualizar tarefa" : "Adicionar Tarefa"}</button>
-        <hr/>
-        
-        {tarefas.map( (item, index) =>(
+
+        <strong>Você tem: {totalTarefas} tarefas!</strong>
+
+        {tarefas.map((item, index) => (
           <section key={item}>
             <span>{item}</span>
-            <button onClick={ () => excluir(item) }>Excluir</button>
-            <button onClick={ () => editar(item)}>Editar</button>
+            <button onClick={() => excluir(item)}>Excluir</button>
+            <button onClick={() => editar(item)}>Editar</button>
           </section>
-
         ))}
       </div>
   )
